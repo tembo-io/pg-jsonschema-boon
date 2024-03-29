afdbd61045df61465fbe71af07dacac47ac758aa
David E. Wheeler <david@justatheory.com>
2024-03-29 18:16:10 -0400

    Implement schema validation

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,158 @@
-use boon::{Compiler, Draft, Schemas};
+use boon::{Compiler, Schemas};
 use pgrx::prelude::*;
 use serde_json::Value;
 
 pgrx::pg_module_magic!();
 
-#[pg_extern]
-fn jsonschema_validate(jsonb: pgrx::JsonB, schemas: Vec<pgrx::JsonB>) -> bool {
-    let schema_url = "http://tmp/schema.json";
-    let instance: Value =
-        serde_json::from_value(jsonb.0).expect("failed to parse json response from SPI");
+// pg_jsonschema-compatible functions.
+#[pg_extern(immutable, strict)]
+fn json_matches_schema(schema: pgrx::Json, instance: pgrx::Json) -> bool {
+    let schemas = [schema.0];
+    validate(id_for(&schemas), &schemas, instance.0)
+}
 
-    // Create the compiler. Default to the latest draft; but add a GUC.
+#[pg_extern(immutable, strict)]
+fn jsonb_matches_schema(schema: pgrx::Json, instance: pgrx::JsonB) -> bool {
+    let schemas = [schema.0];
+    validate(id_for(&schemas), &schemas, instance.0)
+}
+
+#[pg_extern(immutable, strict)]
+fn jsonschema_is_valid(schema: pgrx::Json) -> bool {
+    let schemas = [schema.0];
+    compiles(id_for(&schemas), &schemas)
+}
+
+// New functions.
+#[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
+fn jsonb_schema_is_valid(schema: pgrx::JsonB) -> bool {
+    let schemas = [schema.0];
+    compiles(id_for(&schemas), &schemas)
+}
+
+#[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
+fn jsonb_schemas_are_valid(schemas: pgrx::Array<pgrx::JsonB>) -> bool {
+    let schemas = convert_jsonbs(schemas);
+    compiles(id_for(&schemas), &schemas)
+}
+
+#[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
+fn json_schemas_are_valid(schemas: pgrx::Array<pgrx::Json>) -> bool {
+    let schemas = convert_jsons(schemas);
+    compiles(id_for(&schemas), &schemas)
+}
+
+#[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
+fn jsonb_schema_id_is_valid(id: &str, schemas: pgrx::Array<pgrx::JsonB>) -> bool {
+    let schemas = convert_jsonbs(schemas);
+    compiles(id, &schemas)
+}
+
+#[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
+fn json_schema_id_is_valid(id: &str, schemas: pgrx::Array<pgrx::Json>) -> bool {
+    let schemas = convert_jsons(schemas);
+    compiles(id, &schemas)
+}
+
+fn convert_jsons(schemas: pgrx::Array<pgrx::Json>) -> Vec<Value> {
+    schemas
+        .iter()
+        .map(|x| match x {
+            Some(s) => s.0,
+            None => {
+                error!("NULL schema");
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn convert_jsonbs(schemas: pgrx::Array<pgrx::JsonB>) -> Vec<Value> {
+    schemas
+        .iter()
+        .map(|x| match x {
+            Some(s) => s.0,
+            None => {
+                error!("NULL schema");
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn id_for(schemas: &[Value]) -> &str {
+    for s in schemas.iter() {
+        if let Value::String(s) = &s["$id"] {
+            return s;
+        };
+    }
+    "schema.json"
+}
+
+fn new_compiler(id: &str, schemas: &[Value]) -> Option<Compiler> {
     let mut compiler = Compiler::new();
-    compiler.set_default_draft(Draft::V2020_12);
 
-    // Load all of the schemas.
-    for s in schemas.into_iter() {
-        let schema: Value =
-            serde_json::from_value(s.0).expect("failed to parse json response from SPI");
-        compiler
-            .add_resource(schema_url, schema)
-            .expect("failed to parse json response from SPI");
+    if schemas.is_empty() {
+        // notice!("{e}");
+        println!("No schemas passed to jsonschema_compile");
+        return None;
     }
 
-    let mut schemas = Schemas::new();
-    let sch_index = compiler
-        .compile(schema_url, &mut schemas)
-        .expect("Cannot compile schemas");
-    let result = schemas.validate(&instance, sch_index);
-    result.is_ok()
+    for (i, s) in schemas.iter().enumerate() {
+        let sid = if let Value::String(s) = &s["$id"] {
+            s.to_string()
+        } else {
+            // Use id for the first item and loc{i} for others.
+            format!(
+                "{id}{}",
+                if i == 0 {
+                    "".to_string()
+                } else {
+                    i.to_string()
+                }
+            )
+        };
+
+        if let Err(e) = compiler.add_resource(&sid, s.clone()) {
+            // notice!("{e}");
+            println!("{e}");
+            return None;
+        }
+    }
+
+    Some(compiler)
+}
+
+fn compiles(id: &str, schemas: &[Value]) -> bool {
+    if let Some(mut c) = new_compiler(id, schemas) {
+        let mut schemas = Schemas::new();
+        if let Err(e) = c.compile(id, &mut schemas) {
+            // notice!("{e}");
+            println!("{e}");
+            return false;
+        }
+        return true;
+    }
+
+    false
+}
+
+pub fn validate(id: &str, schemas: &[Value], instance: Value) -> bool {
+    if let Some(mut c) = new_compiler(id, schemas) {
+        let mut schemas = Schemas::new();
+        match c.compile(id, &mut schemas) {
+            Err(e) => {
+                println!("{e}");
+                return false;
+            }
+            Ok(index) => {
+                if let Err(e) = schemas.validate(&instance, index) {
+                    println!("{e}");
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+    false
 }
 
 #[cfg(any(test, feature = "pg_test"))]
@@ -39,9 +163,9 @@ mod tests {
 
     #[pg_test]
     fn test_hello_jsonschema() -> Result<(), Box<dyn Error>> {
-        assert!(crate::jsonschema_validate(
+        assert!(crate::jsonb_matches_schema(
+            pgrx::Json(serde_json::from_str(r#"{"type": "object"}"#)?),
             pgrx::JsonB(serde_json::from_str(r#"{"type": "object"}"#)?),
-            vec![pgrx::JsonB(serde_json::from_str(r#"{"type": "object"}"#)?)],
         ));
 
         Ok(())
