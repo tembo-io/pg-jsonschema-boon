aa094f03718aeccb9d5c01b07e08ae4df658446d
David E. Wheeler <david@justatheory.com>
2024-03-30 18:42:53 -0400

    Return results

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use boon::{Compiler, Schemas};
+use boon::{CompileError, Compiler, Schemas};
 use pgrx::prelude::*;
 use serde_json::Value;
 
@@ -28,13 +28,19 @@ macro_rules! values_for {
 #[pg_extern(immutable, strict)]
 fn json_matches_schema(schema: pgrx::Json, instance: pgrx::Json) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, instance.0)
+    match validate(id_for!(&schemas[0]), &schemas, instance.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 #[pg_extern(immutable, strict)]
 fn jsonb_matches_schema(schema: pgrx::Json, instance: pgrx::JsonB) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, instance.0)
+    match validate(id_for!(&schemas[0]), &schemas, instance.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 // Schema validation functions.
@@ -48,14 +54,20 @@ fn jsonb_matches_schema(schema: pgrx::Json, instance: pgrx::JsonB) -> bool {
 #[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
 fn json_schema_is_valid(schema: pgrx::Json) -> bool {
     let schemas = [schema.0];
-    compiles(id_for!(&schemas[0]), &schemas)
+    match compiles(id_for!(&schemas[0]), &schemas) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_is_valid validates `schema`.
 #[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
 fn jsonb_schema_is_valid(schema: pgrx::JsonB) -> bool {
     let schemas = [schema.0];
-    compiles(id_for!(&schemas[0]), &schemas)
+    match compiles(id_for!(&schemas[0]), &schemas) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// json_schema_id_is_valid validates the schema with the `$id` `id` from the
@@ -63,7 +75,10 @@ fn jsonb_schema_is_valid(schema: pgrx::JsonB) -> bool {
 #[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
 fn json_schema_id_is_valid(id: &str, schemas: pgrx::VariadicArray<pgrx::Json>) -> bool {
     let schemas = values_for!(schemas);
-    compiles(id, &schemas)
+    match compiles(id, &schemas) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_id_is_valid validates the schema with the `$id` `id` from the
@@ -71,7 +86,10 @@ fn json_schema_id_is_valid(id: &str, schemas: pgrx::VariadicArray<pgrx::Json>) -
 #[pg_extern(immutable, strict, name = "jsonschema_is_valid")]
 fn jsonb_schema_id_is_valid(id: &str, schemas: pgrx::VariadicArray<pgrx::JsonB>) -> bool {
     let schemas = values_for!(schemas);
-    compiles(id, &schemas)
+    match compiles(id, &schemas) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 // Document validation functions.
@@ -85,28 +103,40 @@ fn jsonb_schema_id_is_valid(id: &str, schemas: pgrx::VariadicArray<pgrx::JsonB>)
 #[pg_extern(immutable, strict, name = "jsonschema_validates")]
 fn json_schema_validates_json(json: pgrx::Json, schema: pgrx::Json) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, json.0)
+    match validate(id_for!(&schemas[0]), &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_validates_jsonb validates `json` against `schema`.
 #[pg_extern(immutable, strict, name = "jsonschema_validates")]
 fn jsonb_schema_validates_jsonb(json: pgrx::JsonB, schema: pgrx::JsonB) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, json.0)
+    match validate(id_for!(&schemas[0]), &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// json_schema_validates_jsonb validates `json` against `schema`.
 #[pg_extern(immutable, strict, name = "jsonschema_validates")]
 fn json_schema_validates_jsonb(json: pgrx::Json, schema: pgrx::JsonB) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, json.0)
+    match validate(id_for!(&schemas[0]), &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_validates_json validates `json` against `schema`.
 #[pg_extern(immutable, strict, name = "jsonschema_validates")]
 fn jsonb_schema_validates_json(json: pgrx::JsonB, schema: pgrx::Json) -> bool {
     let schemas = [schema.0];
-    validate(id_for!(&schemas[0]), &schemas, json.0)
+    match validate(id_for!(&schemas[0]), &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 // jsonschema_validates(doc::json,  id::text, VARIADIC schema::json)
@@ -123,7 +153,10 @@ fn json_schema_id_validates_json(
     schemas: pgrx::VariadicArray<pgrx::Json>,
 ) -> bool {
     let schemas = values_for!(schemas);
-    validate(id, &schemas, json.0)
+    match validate(id, &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_id_validates_jsonb validates `json` against the schema with
@@ -135,7 +168,10 @@ fn jsonb_schema_id_validates_jsonb(
     schemas: pgrx::VariadicArray<pgrx::JsonB>,
 ) -> bool {
     let schemas = values_for!(schemas);
-    validate(id, &schemas, json.0)
+    match validate(id, &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// json_schema_id_validates_jsonb validates `json` against the schema with
@@ -147,7 +183,10 @@ fn json_schema_id_validates_jsonb(
     schemas: pgrx::VariadicArray<pgrx::JsonB>,
 ) -> bool {
     let schemas = values_for!(schemas);
-    validate(id, &schemas, json.0)
+    match validate(id, &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// jsonb_schema_id_validates_json validates `json` against the schema with
@@ -159,14 +198,17 @@ fn jsonb_schema_id_validates_json(
     schemas: pgrx::VariadicArray<pgrx::Json>,
 ) -> bool {
     let schemas = values_for!(schemas);
-    validate(id, &schemas, json.0)
+    match validate(id, &schemas, json.0) {
+        Err(e) => error!("{e}"),
+        Ok(ok) => ok,
+    }
 }
 
 /// new_compiler creates and returns a new `boon::Compiler` loaded with
 /// `schemas`. Each schema in `schemas` is named for its `$id` field or, if it
 /// has none, `id` is used for the first schema, and `"{id}{i}"` for
 /// subsequent schemas.
-fn new_compiler(id: &str, schemas: &[Value]) -> Option<Compiler> {
+fn new_compiler(id: &str, schemas: &[Value]) -> Result<Compiler, CompileError> {
     let mut compiler = Compiler::new();
 
     if schemas.is_empty() {
@@ -184,45 +226,42 @@ fn new_compiler(id: &str, schemas: &[Value]) -> Option<Compiler> {
             format!("{id}{i}")
         };
 
-        if let Err(e) = compiler.add_resource(&sid, s.clone()) {
-            error!("{e}");
-        }
+        compiler.add_resource(&sid, s.to_owned())?;
     }
 
-    Some(compiler)
+    Ok(compiler)
 }
 
 /// compiles compiles the schema named `id` in `schemas`, returning `true` on
 /// success and `false` on failure.
-fn compiles(id: &str, schemas: &[Value]) -> bool {
-    if let Some(mut c) = new_compiler(id, schemas) {
+fn compiles(id: &str, schemas: &[Value]) -> Result<bool, CompileError> {
+    if let Ok(mut c) = new_compiler(id, schemas) {
         let mut schemas = Schemas::new();
-        if let Err(e) = c.compile(id, &mut schemas) {
-            error!("{e}");
-        }
-        return true;
+        c.compile(id, &mut schemas)?;
+        return Ok(true);
     }
 
-    false
+    Ok(false)
 }
 
 /// validate validates `instance` against schema `id` in `schemas`.
-pub fn validate(id: &str, schemas: &[Value], instance: Value) -> bool {
-    if let Some(mut c) = new_compiler(id, schemas) {
-        let mut schemas = Schemas::new();
-        match c.compile(id, &mut schemas) {
-            Err(e) => error!("{e}"),
-            Ok(index) => {
-                if let Err(e) = schemas.validate(&instance, index) {
-                    info!("{e}");
-                    return false;
+pub fn validate(id: &str, schemas: &[Value], instance: Value) -> Result<bool, CompileError> {
+    match new_compiler(id, schemas) {
+        Err(e) => Err(e),
+        Ok(mut c) => {
+            let mut schemas = Schemas::new();
+            match c.compile(id, &mut schemas) {
+                Err(e) => Err(e),
+                Ok(index) => {
+                    if let Err(e) = schemas.validate(&instance, index) {
+                        info!("{e}");
+                        return Ok(false);
+                    }
+                    Ok(true)
                 }
-                return true;
             }
         }
     }
-
-    false
 }
 
 #[cfg(any(test, feature = "pg_test"))]
@@ -233,19 +272,32 @@ mod tests {
     use serde_json::json;
     use std::{env, error::Error, fs::File, path::Path};
 
-    #[pg_test]
-    fn test_jsonschema_is_valid() -> Result<(), Box<dyn Error>> {
+    fn load_json(name: &str) -> Value {
         let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         let root = Path::new(&root_dir);
-        let address = serde_json::from_reader(File::open(
-            root.join("schemas").join("address.schema.json"),
-        )?)?;
-        let user = serde_json::from_reader(File::open(
-            root.join("schemas").join("user-profile.schema.json"),
-        )?)?;
-        let id = "https://example.com/user-profile.schema.json";
+        serde_json::from_reader(File::open(root.join("schemas").join(name)).unwrap()).unwrap()
+    }
 
-        assert!(compiles(id, &[address, user]));
+    #[pg_test]
+    fn test_compiles() -> Result<(), Box<dyn Error>> {
+        let address = load_json("address.schema.json");
+        let user = load_json("user-profile.schema.json");
+
+        assert!(compiles(
+            "https://example.com/address.schema.json",
+            &[address.clone(), user.clone()]
+        )
+        .unwrap());
+        assert!(compiles(
+            "https://example.com/user-profile.schema.json",
+            &[address.clone(), user.clone()]
+        )
+        .unwrap());
+        assert!(compiles(
+            "https://example.com/nonesuch.schema.json",
+            &[address.clone(), user.clone()]
+        )
+        .is_err());
 
         Ok(())
     }
